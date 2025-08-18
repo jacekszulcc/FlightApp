b5d9e900231299d80a1fd7b68c0a6e1da44a716c
jacekszulc <jacek@szulc.cc>
2025-08-18 18:54:50 +0200

    feat: Implement Global Exception Handling (closes #9)

---
+++ src/main/java/cc/szulc/flightapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package cc.szulc.flightapp.exception;
+
+import cc.szulc.flightapp.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handle(Exception ex) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal Server Error",
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+}
